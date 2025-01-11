d3d136d91fcc49bbbcc211d4c4293da9b4d53008
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-01-11 16:29:01 +0200

    update

--- src/main/java/daniel/nuud/reservationsystem/user/mapper/JsonNullableMapper.java
+++ src/main/java/daniel/nuud/reservationsystem/user/mapper/JsonNullableMapper.java
@@ -3,21 +3,22 @@ package daniel.nuud.reservationsystem.user.mapper;
 import org.mapstruct.Condition;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
+import org.openapitools.jackson.nullable.JsonNullable;
 
 @Mapper(
         componentModel = MappingConstants.ComponentModel.SPRING
 )
-public interface JsonNullableMapper {
-//    public <T> JsonNullable<T> wrap(T entity) {
-//        return JsonNullable.of(entity);
-//    }
-//
-//    public <T> T unwrap(JsonNullable<T> jsonNullable) {
-//        return jsonNullable == null ? null : jsonNullable.orElse(null);
-//    }
-//
-//    @Condition
-//    public <T> boolean isPresent(JsonNullable<T> nullable) {
-//        return nullable != null && nullable.isPresent();
-//    }
+public abstract class JsonNullableMapper {
+    public <T> JsonNullable<T> wrap(T entity) {
+        return JsonNullable.of(entity);
+    }
+
+    public <T> T unwrap(JsonNullable<T> jsonNullable) {
+        return jsonNullable == null ? null : jsonNullable.orElse(null);
+    }
+
+    @Condition
+    public <T> boolean isPresent(JsonNullable<T> nullable) {
+        return nullable != null && nullable.isPresent();
+    }
 }
