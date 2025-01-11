5db279b7013688dc59cd78ae826654ba761e28de
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-01-11 06:30:58 +0200

    updated UserDTO

--- src/main/java/daniel/nuud/reservationsystem/user/dto/UserDTO.java
+++ src/main/java/daniel/nuud/reservationsystem/user/dto/UserDTO.java
@@ -1,8 +1,13 @@
 package daniel.nuud.reservationsystem.user.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserDTO {
     private String firstName;
     private String lastName;
     private String email;
-    private String phone;
+    private String phoneNumber;
 }
