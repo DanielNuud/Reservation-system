50b36c823c9ae20b5221c9f3e0bc6be3629d89bc
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-01-11 06:31:37 +0200

    update JsonNullableMapper

--- src/main/java/daniel/nuud/reservationsystem/user/mapper/JsonNullableMapper.java
+++ src/main/java/daniel/nuud/reservationsystem/user/mapper/JsonNullableMapper.java
@@ -8,16 +8,16 @@ import org.mapstruct.MappingConstants;
         componentModel = MappingConstants.ComponentModel.SPRING
 )
 public interface JsonNullableMapper {
-    public <T> JsonNullable<T> wrap(T entity) {
-        return JsonNullable.of(entity);
-    }
-
-    public <T> T unwrap(JsonNullable<T> jsonNullable) {
-        return jsonNullable == null ? null : jsonNullable.orElse(null);
-    }
-
-    @Condition
-    public <T> boolean isPresent(JsonNullable<T> nullable) {
-        return nullable != null && nullable.isPresent();
-    }
+//    public <T> JsonNullable<T> wrap(T entity) {
+//        return JsonNullable.of(entity);
+//    }
+//
+//    public <T> T unwrap(JsonNullable<T> jsonNullable) {
+//        return jsonNullable == null ? null : jsonNullable.orElse(null);
+//    }
+//
+//    @Condition
+//    public <T> boolean isPresent(JsonNullable<T> nullable) {
+//        return nullable != null && nullable.isPresent();
+//    }
 }
