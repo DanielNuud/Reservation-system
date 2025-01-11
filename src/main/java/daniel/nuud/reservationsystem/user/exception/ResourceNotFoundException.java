193fe922068bd2b11110ca04eedd9b8d45afad97
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-01-11 06:31:49 +0200

    create ResourceNotFoundException

---
+++ src/main/java/daniel/nuud/reservationsystem/user/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package daniel.nuud.reservationsystem.user.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
