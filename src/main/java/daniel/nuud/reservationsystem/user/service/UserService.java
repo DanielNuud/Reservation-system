b207e23d7cb9e002c71e90f3ab2ab413c3c0bde9
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-01-11 06:30:22 +0200

    added 2 methods

--- src/main/java/daniel/nuud/reservationsystem/user/service/UserService.java
+++ src/main/java/daniel/nuud/reservationsystem/user/service/UserService.java
@@ -1,9 +1,12 @@
 package daniel.nuud.reservationsystem.user.service;
 
+import daniel.nuud.reservationsystem.user.dto.UserDTO;
 import daniel.nuud.reservationsystem.user.mapper.UserMapper;
 import daniel.nuud.reservationsystem.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -14,6 +17,17 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    public UserDTO createUser(UserDTO userDTO) {
+        var user = userMapper.toEntity(userDTO);
+        userRepository.save(user);
+        return userMapper.toDTO(user);
+    }
 
+    public List<UserDTO> getAllUsers() {
+        var users = userRepository.findAll();
+        var result = users.stream().map(user -> userMapper.toDTO(user))
+                .toList();
+        return result;
+    }
 
 }
