06b6b8510de94efa3fcbb7ff16115bd1b0125b81
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-01-13 22:49:40 +0200

    create OrderCreateDTO

---
+++ src/main/java/daniel/nuud/reservationsystem/order/dto/OrderCreateDTO.java
@@ -0,0 +1,23 @@
+package daniel.nuud.reservationsystem.order.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+public class OrderCreateDTO {
+    @NotNull(message = "User ID must not be null")
+    private Long userId;
+
+    @NotNull(message = "House ID must not be null")
+    private Long houseId;
+
+    @NotNull(message = "Start date must not be null")
+    private Instant startReservation;
+
+    @NotNull(message = "End date must not be null")
+    private Instant endReservation;
+}
