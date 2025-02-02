c0479cf3bd6dad30f501512b8b4c0e34c1b4c4d9
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-02-03 00:23:23 +0200

    added new methods

--- src/main/java/daniel/nuud/reservationsystem/controller/OrderController.java
+++ src/main/java/daniel/nuud/reservationsystem/controller/OrderController.java
@@ -7,7 +7,9 @@ import daniel.nuud.reservationsystem.model.OrderEntity;
 import daniel.nuud.reservationsystem.service.HouseService;
 import daniel.nuud.reservationsystem.service.OrderService;
 import daniel.nuud.reservationsystem.service.UserService;
+import daniel.nuud.reservationsystem.util.WebUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -15,7 +17,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class OrderController {
@@ -31,8 +38,7 @@ public class OrderController {
 
     @GetMapping("/orders")
     public String orders(Model model) {
-        final List<OrderDTO> list = orderService.getAllOrders();
-        model.addAttribute("orders", list);
+        model.addAttribute("orders", orderService.getListGroupByCities());
         return "order/list";
     }
 
@@ -48,21 +54,51 @@ public class OrderController {
         return "order/view";
     }
 
-    @PostMapping
+    @PostMapping("/houses/{houseId}/book")
     public String createOrder(@PathVariable Long houseId,
                               @ModelAttribute OrderCreateDTO orderCreateDTO,
+                              @RequestParam("checkIn") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDate startReservation,
+                              @RequestParam("checkOut") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDate endReservation,
                               @AuthenticationPrincipal UserDetails userDetails,
                               RedirectAttributes redirectAttributes) {
+
         var user = userService.findByUserName(userDetails.getUsername());
         var house = houseService.findHouseById(houseId);
 
+        Instant start = startReservation.atStartOfDay(ZoneId.systemDefault()).toInstant();
+        Instant end = endReservation.atStartOfDay(ZoneId.systemDefault()).toInstant();
+
+        orderCreateDTO.setStartReservation(start);
+        orderCreateDTO.setEndReservation(end);
         orderCreateDTO.setHouseId(house.getId());
         orderCreateDTO.setUserId(user.getId());
 
         orderService.createOrder(orderCreateDTO);
-        redirectAttributes.addFlashAttribute("success", "Reservation successful!");
+        redirectAttributes.addFlashAttribute(WebUtils.MSG_SUCCESS,  WebUtils.getMessage("order.create.success"));
 
-        return "redirect:/houses";
+        return "redirect:/orders";
+    }
+
+    @PostMapping("/orders/cancel/{id}")
+    public String cancelOrder(@PathVariable(name = "id") Long id, RedirectAttributes redirectAttributes) {
+
+        try {
+            orderService.cancelOrder(id);
+            redirectAttributes.addFlashAttribute(WebUtils.MSG_SUCCESS,  WebUtils.getMessage("order.cancel.success"));
+        } catch (IllegalStateException e) {
+            redirectAttributes.addFlashAttribute(WebUtils.MSG_ERROR, "Order is already canceled.");
+        } catch (RuntimeException e) {
+            redirectAttributes.addFlashAttribute(WebUtils.MSG_ERROR, WebUtils.getMessage(e.getMessage()));
+        }
+
+        return "redirect:/orders";
+    }
+
+    @PostMapping("/orders/{id}")
+    public String deleteOrder(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        orderService.deleteOrder(id);
+        redirectAttributes.addFlashAttribute(WebUtils.MSG_SUCCESS, "Order successfully deleted!");
+        return "redirect:/orders";
     }
 
 }
